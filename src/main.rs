3c316c483a4c8506d1e3e83645a18843c2170b57
interrrp <int@addictedto.coffee>
2023-10-31 07:46:20 +0800

    Create single-threaded web server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{
-    fs,
-    io::{self, Write},
-    net::TcpListener,
-};
+use std::{fs, io, net::TcpListener};
 
+use brainfuck::{program::Program, tape::ArrayTape, Interpreter};
 use clap::Parser;
 
 use bfss::{Args, Header};
@@ -14,9 +11,20 @@ fn main() -> io::Result<()> {
     let contents = fs::read_to_string(&args.file)?;
     let header = Header::from_brainfuck(&contents);
 
+    println!("Serving {} on http://{}", args.file, header.address);
+
     let listener = TcpListener::bind(header.address)?;
     for stream in listener.incoming() {
-        stream?.write_all(b"OK")?;
+        // Since `Program` unfortunately does not derive from Clone, we have to construct one on
+        // every connection
+
+        let mut stream = stream?;
+        let mut stream_clone = stream.try_clone()?;
+
+        let program = Program::parse(&contents).unwrap();
+
+        let mut interp = Interpreter::<ArrayTape>::new(program, &mut stream, &mut stream_clone);
+        interp.run().unwrap();
     }
 
     Ok(())
