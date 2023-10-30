026e68c3af341d94e9dc2aeffbb32430974487e5
interrrp <int@addictedto.coffee>
2023-10-31 07:58:59 +0800

    Make asynchronous

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
-use std::{fs, io, net::TcpListener};
+use std::io::{Read, Write};
 
-use brainfuck::{program::Program, tape::ArrayTape, Interpreter};
+use tokio::{fs, io, net::TcpListener};
+
+use brainfuck::{program::Program, tape::ArrayTape, Error, Interpreter};
 use clap::Parser;
 
 use bfss::{Args, Header};
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let contents = fs::read_to_string(&args.file)?;
+    let contents = fs::read_to_string(&args.file).await?;
     let header = Header::from_brainfuck(&contents);
 
     println!("Serving {} on http://{}", args.file, header.address);
 
-    let listener = TcpListener::bind(header.address)?;
-    for stream in listener.incoming() {
-        // Since `Program` unfortunately does not derive from Clone, we have to construct one on
-        // every connection
-
-        let mut stream = stream?;
-        let mut stream_clone = stream.try_clone()?;
-
-        let program = Program::parse(&contents).unwrap();
-
-        let mut interp = Interpreter::<ArrayTape>::new(program, &mut stream, &mut stream_clone);
-        interp.run().unwrap();
+    let listener = TcpListener::bind(header.address).await?;
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let mut stream = stream.into_std().unwrap();
+        let mut stream_clone = stream.try_clone().unwrap();
+        let contents = contents.clone();
+        tokio::spawn(async move {
+            eval_brainfuck(&contents, &mut stream, &mut stream_clone).unwrap();
+        });
     }
+}
 
-    Ok(())
+fn eval_brainfuck(code: &str, input: &mut impl Read, output: &mut impl Write) -> Result<(), Error> {
+    let program = Program::parse(code).unwrap();
+    let mut interp = Interpreter::<ArrayTape>::new(program, input, output);
+    interp.run()
 }
